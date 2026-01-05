9da8499a9c1c740be7734ab597c5da1fb17fbea7
Seylunn <easyby14@gmail.com>
2026-01-06 01:41:49 +1100

    Update index.js

--- index.js
+++ index.js
@@ -307,8 +307,6 @@ const HELP_CATEGORIES = {
       { name: ',ownerinfo', desc: 'show the owners info' },
       { name: ',memberdm', desc: 'DM any user with the command' },
       { name: ',servericon', desc: 'show the servers icon' },
-      { name: ',setfm', desc: 'link your last fm account' }, 
-      { name: ',fm', desc: 'shows what your playing with last fm' },
       { name: ',uptime', desc: 'Bot uptime' }
     ]
   },
@@ -327,7 +325,6 @@ const HELP_CATEGORIES = {
     title: 'Leaderboard Commands',
     commands: [
       { name: ',msglb', desc: 'Message leaderboard' },
-      { name: ',fmlb', desc: 'Show last fm scrobbles' },
       { name: ',afklb', desc: 'AFK leaderboard' }
     ]
   },
@@ -2463,6 +2460,7 @@ client.on('interactionCreate', async (interaction) => {
 // ===================== LOGIN ===================== //
 
 client.login(TOKEN);
+
 
 
 
